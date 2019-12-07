55395b2248113a2805eb5ea64bc262e89e5a8a8b
github username <github email address>
2019-12-07 15:18:24 +0000

    Global variables have been added to the top of the index.js file.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
-  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+  app = express().use(bodyParser.json()), // creates express http server
+  PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
